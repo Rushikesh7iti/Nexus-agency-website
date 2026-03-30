7eb5255ea9e5dbd9823aa7db49f624e0e9706d7e
rushikesh kuruba <rushikeshkuruba@gmail.com>
2026-03-31 00:32:58 +0530

    fix: Update script.js endpoints to point correctly

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ document.addEventListener('DOMContentLoaded', () => {
             };
 
             try {
-                const response = await fetch('http://localhost:5000/api/contact', {
+                const response = await fetch('https://nexus-backend-dgqw.onrender.com/api/contact', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
